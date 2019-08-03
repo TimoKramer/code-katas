e130b9e3d6360a0c8978841c05e7cd7887c8688e
Timo Kramer <fw-git@timokramer.de>
2019-08-03 16:46:30 +0200

    parsing chars

--- bankocr.py
+++ bankocr.py
@@ -2,19 +2,31 @@
 
 import itertools
 
-def take_lines(number, filehandle):
-    return itertools.islice(filehandle, number)
+def take_lines(filehandle):
+    return itertools.islice(filehandle, 4)
 
-def take_char(char_length, lines):
+def take_char(lines):
     for line in lines:
-        yield itertools.islice(line, char_length)
+        yield itertools.islice(line, 3)
+
+def parse_char(stringed_char):
+    switcher = {
+            '     |  |': 1,
+            ' _  _||_ ': 2
+            }
+    return switcher.get(stringed_char, None)
+
+def process(char):
+    flattened_char = [y for x in char for y in x]
+    stringed_char = ''.join(flattened_char).strip('\n')
+    parsed_char = parse_char(stringed_char)
+    print(parsed_char)
 
 def char_recon(file):
     with open(file) as f:
-        lines = take_lines(4, f)
-        char = take_char(3, lines)
-        for i in char:
-            print(list(i))
+        lines = take_lines(f)
+        raw_char = take_char(lines)
+        processed_char = process(raw_char)
 
 if __name__ == '__main__':
     char_recon('test.txt')
--- test_bankocr.py
+++ test_bankocr.py
@@ -8,14 +8,19 @@ import bankocr
 
 class TestBankOCR(unittest.TestCase):
 
-    def test_take_lines_instance(self):
-        self.assertIsInstance(bankocr.take_lines(3, 'test.txt'), itertools.islice)
-
-    def test_take_4_lines(self):
-        self.assertEqual(len(list(bankocr.take_lines(4, 'test.txt'))), 4)
+    def test_take_lines(self):
+        with open('test.txt') as f:
+            self.assertIsInstance(bankocr.take_lines(f), itertools.islice)
+            self.assertEquals(list(bankocr.take_lines(f))[1], '  | _| _||_||_ |_   ||_||_|\n')
 
     def test_char(self):
-        self.assertEqual(list(list(bankocr.take_char(3, ['foobar']))[0]), ['f', 'o', 'o'])
+        self.assertEqual(list(list(bankocr.take_char(['foobar']))[0]), ['f', 'o', 'o'])
+        self.assertEqual(list(list(bankocr.take_char(['foobar']))[0]), ['f', 'o', 'o'])
+
+    def test_parse_char(self):
+        self.assertIsNot(bankocr.parse_char('     |  |'), 2)
+        self.assertIsNone(bankocr.parse_char('        |'), 2)
+        self.assertIs(bankocr.parse_char(' _  _||_ '), 2)
 
 if __name__ == '__main__':
     unittest.main()

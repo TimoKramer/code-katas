b31e8a1473fb66622cce7487fc4293debf4eff24
Timo Kramer <fw-git@timokramer.de>
2019-08-04 20:55:05 +0200

    did not like the solution

--- bankocr.py
+++ bankocr.py
@@ -1,53 +1,24 @@
 #!/usr/bin/env python3
 
-import itertools
-
-
-def take_lines(filehandle):
-    try:
-        lines = itertools.islice(filehandle, 4)
-    except StopIteration:
-        return None
-    yield lines
-
-
-def take_char(lines):
-    for line in next(lines):
-        try:
-            c = itertools.islice(line, 3)
-        except StopIteration:
-            return None
-        yield c
-
-
-def parse_char(stringed_char):
-    switcher = {
-            '     |  |': 1,
-            ' _ _| |_ ': 2,
-            '   |_|  |': 4
-            }
-    return switcher.get(stringed_char, None)
-
-
-def process(char):
-    listed_char = list(char)
-    flattened_char = [y for x in listed_char for y in x]
-    stringed_char = ''.join(flattened_char).strip('\n')
-    parsed_char = parse_char(stringed_char)
-    return parsed_char
-
-
-def char_recon(file):
-    with open(file) as f:
-        lines = take_lines(f)
-        raw_char = take_char(lines)
-        processed_char = process(raw_char)
-        print(processed_char)
-        lines = take_lines(f)
-        raw_char = take_char(lines)
-        processed_char = process(raw_char)
-        print(processed_char)
-
+def char_recon(filename):
+    matrix = open(filename).readlines()
+    matrix = [item.rstrip('\n') for item in matrix]
+    chars = []
+    for line_block in range(0, len(matrix), 4):
+        print('line_block ' + str(line_block))
+        for char_block in range(0, len(matrix[0]), 3):
+            print('char_block ' + str(char_block))
+            char = []
+            char.append(matrix[line_block][char_block])
+            char.append(matrix[line_block][char_block + 1])
+            char.append(matrix[line_block][char_block + 2])
+            char.append(matrix[line_block + 1][char_block])
+            char.append(matrix[line_block + 1][char_block + 1])
+            char.append(matrix[line_block + 1][char_block + 2])
+            char.append(matrix[line_block + 2][char_block])
+            char.append(matrix[line_block + 2][char_block + 1])
+            char.append(matrix[line_block + 2][char_block + 2])
+            chars.append(char)
 
 if __name__ == '__main__':
     char_recon('test.txt')

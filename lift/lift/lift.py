2d0c5383a85aec6f4927c20f84d060a504f41025
Timo Kramer <fw-git@timokramer.de>
2020-02-25 18:48:52 +0100

    lift responds to call; lift has attribute floor

--- lift/lift/lift.py
+++ lift/lift/lift.py
@@ -1,6 +1,16 @@
+class Lift():
 
-def call_lift(source_floor, direction):
-    return "ok"
+    def __init__(self, current_floor):
+        self.current_floor = current_floor
+
+    def get_current_floor(self):
+        return self.current_floor
+
+    def deliver_to_floor(self, floor):
+        return "ok"
+
+    def call_lift(self, source_floor, direction):
+        return "ok"
 
 
 
--- lift/test/test_lift.py
+++ lift/test/test_lift.py
@@ -1,5 +1,17 @@
 import pytest
 import lift.lift as lift
 
+
+def test_get_current_floor():
+    test_lift = lift.Lift(2)
+    assert test_lift.get_current_floor() == 2
+
+def test_deliver_to_floor():
+    test_lift = lift.Lift(2)
+    assert test_lift.deliver_to_floor(3) == "ok"
+
 def test_call_lift():
-    assert lift.call_lift(3, "down") == "ok"
+    test_lift = lift.Lift(2)
+    assert test_lift.call_lift(3, "down") == "ok"
+
+
